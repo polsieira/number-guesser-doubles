98f58077f9d4a56afa10d69632412c01b48c2fca
posi7790 <polasp17@gmail.com>
2019-07-02 22:20:04 -0600

    Complete random number generator using parseInt() to turn value to number instead of string

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ function getRandInt(min, max) {
 }
 
 var updateButton = document.querySelector(".article__form__button--update");
-var randInt = null;
 
 updateButton.addEventListener('click', function(){ 
-  var min = document.querySelector(".article__form__input--min-range").value;
-  var max = document.querySelector(".article__form__input--max-range").value;
+  var min = parseInt(document.querySelector(".article__form__input--min-range").value);
+  var max = parseInt(document.querySelector(".article__form__input--max-range").value);
   document.querySelector(".bold--min").innerText = min;
   document.querySelector(".bold--max").innerText = max;
   document.querySelector(".article__form__input--min-range").value = "";
@@ -21,3 +20,22 @@ updateButton.addEventListener('click', function(){
   console.log("random = " + randInt)
 })
 
+// Populate Challenger names and guesses
+var submitButton = document.querySelector(".article__div__button--submit");
+
+submitButton.addEventListener('click', function(){
+  challengerOne = document.querySelector(".article__form__input--nameOne").value;
+  challengerTwo = document.querySelector(".article__form__input--nameTwo").value;
+  guessOne = parseInt(document.querySelector(".article__form__input--guessOne").value);
+  guessTwo = parseInt(document.querySelector(".article__form__input--guessTwo").value);
+  document.querySelector(".article__paragraph--challenger-1").innerText = challengerOne;
+  document.querySelector(".article__paragraph--challenger-2").innerText = challengerTwo;
+  document.querySelector(".span--challenger-1").innerText = challengerOne;
+  document.querySelector(".span--challenger-2").innerText = challengerTwo;
+  document.querySelector(".span--number-1").innerText = guessOne;
+  document.querySelector(".span--number-2").innerText = guessTwo;  
+  document.querySelector(".article__form__input--nameOne").value = "";
+  document.querySelector(".article__form__input--nameTwo").value = "";
+  document.querySelector(".article__form__input--guessOne").value = "";
+  document.querySelector(".article__form__input--guessTwo").value = "";
+})
