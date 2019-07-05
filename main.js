9729ade4c3d10136444ab27d45f05110d9dc2793
Sara Karsh <sara@Saras-MacBook-Pro.local>
2019-07-05 15:17:15 -0600

    Organize code in files for readability

--- main.js
+++ main.js
@@ -1,57 +1,19 @@
-// Generate Random Number
-// Update Max and Min in Current Range
+// Variables
+var min = 1;
+var max = 100;
+var nGuesses = 0;
+var randInt = getRandInt(min, max);
+var updateButton = document.querySelector(".article__form__button--update");
+var submitButton = document.querySelector(".article__div__button--submit");
+var clearButton = document.querySelector(".article__div__button--clear");
+var restartButton = document.querySelector(".article__div__button--restart");
 
-// Function for calculating random integer
+
+// Functions 
 function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Default random number in case user doesn't choose range
-var min = 1;
-var max = 100;
-var randInt = getRandInt(min, max);
-
-var updateButton = document.querySelector(".article__form__button--update");
-
-updateButton.addEventListener('click', function(){ 
-  var min = parseInt(document.querySelector(".article__form__input--min-range").value);
-  var max = parseInt(document.querySelector(".article__form__input--max-range").value);
-  document.querySelector(".bold--min").innerText = min;
-  document.querySelector(".bold--max").innerText = max;
-  document.querySelector(".article__form__input--min-range").value = "";
-  document.querySelector(".article__form__input--max-range").value = "";
-  randInt = getRandInt(min, max);
-})
-
-console.log("random = " + randInt)
-
-// Populate Challenger Names, Guesses, and Messages
-var submitButton = document.querySelector(".article__div__button--submit");
-var nGuesses = 0;
-
-submitButton.addEventListener('click', function(){
-  // Populate Names and Guesses
-  challengerOne = document.querySelector(".article__form__input--nameOne").value;
-  challengerTwo = document.querySelector(".article__form__input--nameTwo").value;
-  guessOne = Math.round(parseInt(document.querySelector(".article__form__input--guessOne").value));
-  guessTwo = Math.round(parseInt(document.querySelector(".article__form__input--guessTwo").value));
-  document.querySelector(".article__paragraph--challenger-1").innerText = challengerOne;
-  document.querySelector(".article__paragraph--challenger-2").innerText = challengerTwo;
-  document.querySelector(".span--challenger-1").innerText = challengerOne;
-  document.querySelector(".span--challenger-2").innerText = challengerTwo;
-  // document.querySelector(".span__challenger1-name").innerText = challengerOne.toUpperCase();
-  // document.querySelector(".span__challenger2-name").innerText = challengerTwo.toUpperCase();
-  document.querySelector(".span--number-1").innerText = guessOne;
-  document.querySelector(".span--number-2").innerText = guessTwo; 
-  //Start timer
-  if (nGuesses === 0) {
-    timer = 0;
-    timerId = setInterval(function() {timer++;}, 1000);
-  } 
-  // Increment nGuesses
-  nGuesses ++;
-  console.log(nGuesses)
-  // Populate message
 function makeCard(winner) {
   document.querySelector('.section--right').insertAdjacentHTML('afterbegin', `<article class="article-winner-card">
         <p class="header-for-winner-card">
@@ -70,16 +32,56 @@ function makeCard(winner) {
       <button type="button" class="winner-card-close-button">&times;</button>
     </div>
   </article>`);
-
-  // document.querySelector('.winning-challenger-name').innerText = winner.toUpperCase();
-  // document.querySelector('.total-number-guesses').innerText = nGuesses * 2;
-  // nGuesses = 0;
-  // document.querySelector('.total-time-spent').innerText = (timer / 60).toFixed(2);
-  // clearInterval(timerId);
-  // document.querySelector('.article-winner-card').classList.remove('hidden');
 }
 
+function setTimer() {
+  if (nGuesses === 0) {
+    timer = 0;
+    timerId = setInterval(function() {timer++;}, 1000);
+  }
+}
 
+function clearForm() {
+  document.querySelector(".article__form__input--nameOne").value = "";
+  document.querySelector(".article__form__input--nameTwo").value = "";
+  document.querySelector(".article__form__input--guessOne").value = "";
+  document.querySelector(".article__form__input--guessTwo").value = "";
+}
+
+function clearLatestGuess() {
+  document.querySelector(".span--high-low-1").innerText = "";
+  document.querySelector(".span--high-low-2").innerText = "";  
+  document.querySelector(".span--number-1").innerText = "";  
+  document.querySelector(".span--number-2").innerText = "";  
+}
+
+// Event Listeners
+updateButton.addEventListener('click', function(){ 
+  var min = parseInt(document.querySelector(".article__form__input--min-range").value);
+  var max = parseInt(document.querySelector(".article__form__input--max-range").value);
+  document.querySelector(".bold--min").innerText = min;
+  document.querySelector(".bold--max").innerText = max;
+  document.querySelector(".article__form__input--min-range").value = "";
+  document.querySelector(".article__form__input--max-range").value = "";
+  randInt = getRandInt(min, max);
+})
+
+console.log("random = " + randInt)
+
+submitButton.addEventListener('click', function(){
+  // Populate Names and Guesses
+  challengerOne = document.querySelector(".article__form__input--nameOne").value;
+  challengerTwo = document.querySelector(".article__form__input--nameTwo").value;
+  guessOne = Math.round(parseInt(document.querySelector(".article__form__input--guessOne").value));
+  guessTwo = Math.round(parseInt(document.querySelector(".article__form__input--guessTwo").value));
+  document.querySelector(".article__paragraph--challenger-1").innerText = challengerOne;
+  document.querySelector(".article__paragraph--challenger-2").innerText = challengerTwo;
+  document.querySelector(".span--challenger-1").innerText = challengerOne;
+  document.querySelector(".span--challenger-2").innerText = challengerTwo;
+  document.querySelector(".span--number-1").innerText = guessOne;
+  document.querySelector(".span--number-2").innerText = guessTwo; 
+  setTimer()
+  nGuesses ++;
 
   if (guessOne > randInt) {
     document.querySelector(".span--high-low-1").innerText = "that's too high";
@@ -106,37 +108,17 @@ function makeCard(winner) {
   } else {
     clearButton.disabled = false;
   // Enable restart button
+  }
   if (nGuesses >= 1) {
     restartButton.disabled = false;
   }
-
-  }
-})
-
-// Clear Challenger Names and Guesses
-var clearButton = document.querySelector(".article__div__button--clear");
-
-function clearForm() {
-  document.querySelector(".article__form__input--nameOne").value = "";
-  document.querySelector(".article__form__input--nameTwo").value = "";
-  document.querySelector(".article__form__input--guessOne").value = "";
-  document.querySelector(".article__form__input--guessTwo").value = "";
-}
-
-function clearLatestGuess() {
-  document.querySelector(".span--high-low-1").innerText = "";
-  document.querySelector(".span--high-low-2").innerText = "";  
-  document.querySelector(".span--number-1").innerText = "";  
-  document.querySelector(".span--number-2").innerText = "";  
-}
+});
 
 clearButton.addEventListener('click', function(){
   clearForm();
   clearLatestGuess();
   clearButton.disabled = true;
 })
-// Restart Game Button
-var restartButton = document.querySelector(".article__div__button--restart");
 
 restartButton.addEventListener('click', function(){
   clearForm();
